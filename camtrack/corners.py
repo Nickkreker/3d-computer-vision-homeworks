ddadff2c1d9a251bd2fe4503e6cff98eb81648ff
Nickkreker <nick_berezikov@me.com>
2020-10-08 10:09:47 +0300

    fix

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -34,50 +34,62 @@ class _CornerStorageBuilder:
     def build_corner_storage(self):
         return StorageImpl(item[1] for item in sorted(self._corners.items()))
 
-
-def _build_impl(frame_sequence: pims.FramesSequence,
-                builder: _CornerStorageBuilder) -> None:
-    # TODO
-    feature_params = dict(blockSize=7,
+def calc_corners(image, max_level=2, old_corners=None):
+	feature_params = dict(blockSize=7,
                           maxCorners=0,
                           minDistance=7,
                           qualityLevel=0.091,
                           useHarrisDetector=False)
 
+	levels = [frame_sequence[0]]
+    points = np.empty((0, 1, 2), dtype=np.uint8)
+    sizes = np.empty((0), dtype=np.uint8)
+    ids = np.empty((0), dtype=np.uint8)
+
+    if old_corners is not None:
+    	last_id = (prev_points.ids.ravel()[-1] + 1).astype(int)
+    else:
+    	last_id = 0
+
+    for i in range(max_level):
+        levels.append(cv2.pyrDown(levels[-1]))
+
+    for counter, level in enumerate(levels):
+        scale = 2 ** counter
+
+
+        mask = np.zeros_like(level, np.uint8)
+        mask[:] = 255
+        if old_corners is not None:
+            points_on_level = old_corners.points[(old_corners.sizes == 7 * scale).reshape(-1)]
+            for point in points_on_level:
+                x, y = point / scale
+                cv2.circle(mask, (x, y), 7 * scale, 0, -1)
+            
+        new_points = cv2.goodFeaturesToTrack(level, mask=mask, **feature_params)
+
+        if new_points is not None:
+            points = np.append(points, new_points * scale, axis=0)
+            sizes = np.append(sizes, (np.ones(len(new_points), dtype=int) * 7 * scale), axis=0)
+    ids = np.arange(last_id, last_id + len(points))
+    return ids, points, sizes
+
+
+def _build_impl(frame_sequence: pims.FramesSequence,
+                builder: _CornerStorageBuilder) -> None:
+    # TODO
     lk_params = dict(winSize=(15, 15),
                      maxLevel=3,
                      criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 20, 0.02))
 
     # Create corners on two levels
-    max_level = 2
-    levels = [frame_sequence[0]]
-    points = np.empty((0, 1, 2), dtype=np.uint8)
-    sizes = np.empty((0), dtype=np.uint8)
-
-    for i in range(max_level):
-        levels.append(cv2.pyrDown(levels[-1]))
-
-    prev_points = None
-    mask = np.zeros_like(frame_sequence[0], np.uint8)
-    mask[:] = 255
-
-    for counter, level in enumerate(levels):
-        scale = 2 ** counter
-        new_points = cv2.goodFeaturesToTrack(level, mask=None, **feature_params)
-
-        if new_points is not None:
-            points = np.append(points, new_points * scale, axis=0)
-            sizes = np.append(sizes, (np.ones(len(new_points), dtype=int) * 7 * scale), axis=0)
-
+    image_0 = frame_sequence[0]
+    ids, points, sizes = calc_corners(image_0)
     corners = FrameCorners(
-        np.arange(len(points)),
+        ids,
         points,
         sizes
     )
-
-    builder.set_corners_at_frame(0, corners)
-
-    image_0 = frame_sequence[0]
 
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
         next_points, st, err = cv2.calcOpticalFlowPyrLK(np.uint8(image_0 * 255), np.uint8(image_1 * 255),
@@ -89,47 +101,23 @@ def _build_impl(frame_sequence: pims.FramesSequence,
         # corners_1_lvl1 = corners_1_lvl1[error < 1]
         tracked_corners = filter_frame_corners(corners, st.reshape(-1) == 1)
         builder.set_corners_at_frame(frame - 1, tracked_corners)
-
-        levels = [image_1]
-        points = np.empty((0, 1, 2), dtype=np.uint8)
-        sizes = np.empty((0), dtype=np.uint8)
-        ids = np.empty((0), dtype=np.uint8)
-
-        for i in range(max_level):
-            levels.append(cv2.pyrDown(levels[-1]))
     
         prev_points = FrameCorners(
             tracked_corners.ids,
             next_points[st.reshape(-1) == 1],
             tracked_corners.sizes
         )
-    
-        for counter, level in enumerate(levels):
-            scale = 2 ** counter
-            mask = np.zeros_like(level, np.uint8)
-            mask[:] = 255
-            if prev_points is not None:
-                points_on_level = prev_points.points[(prev_points.sizes == 7 * scale).reshape(-1)]
-                for point in points_on_level:
-                    x, y = point / scale
-                    cv2.circle(mask, (x, y), 7 * scale, 0, -1)
-            new_points = cv2.goodFeaturesToTrack(level, mask=mask, **feature_params)
 
-            if new_points is not None:
-                #print(new_points, "\n")
-                points = np.append(points, new_points * scale, axis=0)
-                sizes = np.append(sizes, (np.ones(len(new_points), dtype=int) * 7 * scale))
-        points = np.append(tracked_corners.points.reshape(-1, 2), points.reshape(-1, 2), axis=0)
-        sizes = np.append(tracked_corners.sizes.reshape(-1), sizes, axis=0)
-    
-        last_id = (prev_points.ids.ravel()[-1] + 1).astype(int)
-        ids = np.arange(last_id, last_id + len(points))
+        ids, points, sizes = calc_corners(image_1, old_corners=prev_points)
+        ids = np.append(prev_points.ids.reshape(-1), ids, axis=0)
+        points = np.append(prev_points.points.reshape(-1,2), points.reshape(-1,2), axis=0)
+        sizes = np.append(prev_points.sizes.reshape(-1), sizes, axis=0)
     
         corners = FrameCorners(
-                np.append(prev_points.ids.reshape(-1), ids, axis=0),
-                np.append(prev_points.points.reshape(-1, 2), points.reshape(-1, 2), axis=0),
-                np.append(prev_points.sizes.reshape(-1), sizes, axis=0)
-            )
+        	ids,
+        	points,
+        	sizes
+        )
 
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
