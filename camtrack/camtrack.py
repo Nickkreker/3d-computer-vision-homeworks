a3511504c0de1e8371961b4ab915d4ed012880d5
Nickkreker <nick_berezikov@me.com>
2020-11-09 00:41:40 +0300

    hw2

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -5,6 +5,7 @@ __all__ = [
 ]
 
 from typing import List, Optional, Tuple
+import cv2
 
 import numpy as np
 
@@ -17,8 +18,101 @@ from _camtrack import (
     calc_point_cloud_colors,
     pose_to_view_mat3x4,
     to_opencv_camera_mat3x3,
-    view_mat3x4_to_pose
+    view_mat3x4_to_pose,
+    build_correspondences,
+    triangulate_correspondences,
+    TriangulationParameters,
+    Correspondences,
+    rodrigues_and_translation_to_view_mat3x4,
+    filter_frame_corners
 )
+
+def untracked_frames(view_mats):
+    frames = []
+    for frame, mat in enumerate(view_mats):
+        if mat is None:
+            frames.append(frame)
+    return frames
+
+def calc_camera_pose(frame, corner_storage, cloud, intrinsic_mat):
+    corners = corner_storage[frame]
+    points3d = []
+    points2d = []
+
+    for id, point2d in zip(corners.ids.flatten(), corners.points):
+        if id in cloud.keys():
+            points2d.append(point2d)
+            points3d.append(cloud[id])
+    points3d = np.array(points3d, dtype=np.float64)
+    points2d = np.array(points2d, dtype=np.float64)
+
+    if len(points2d) < 4:
+        return None
+
+    succeeded, r_vec, t_vec, inliers = cv2.solvePnPRansac(objectPoints=points3d,
+                                                          imagePoints=points2d,
+                                                          cameraMatrix=intrinsic_mat,
+                                                          distCoeffs=None,
+                                                          reprojectionError=8.0,
+                                                          confidence=0.999,
+                                                          flags=cv2.SOLVEPNP_EPNP)
+    if succeeded == False:
+        return None
+
+    inliers = np.asarray(inliers, dtype=np.int).flatten()
+    points3d = np.array(points3d)
+    points2d = np.array(points2d)
+
+    _, rvec, tvec = cv2.solvePnP(objectPoints=points3d[inliers],
+                                 imagePoints=points2d[inliers],
+                                 cameraMatrix=intrinsic_mat,
+                                 distCoeffs=None,
+                                 useExtrinsicGuess=True,
+                                 rvec=r_vec,
+                                 tvec=t_vec,
+                                 flags=cv2.SOLVEPNP_ITERATIVE)
+    return rvec, tvec, len(inliers)
+
+
+def add_new_point(corner, frames_of_corner, view_mats, tvecs, corner_storage, cloud, cur_corners_occurencies, intrinsic_mat):
+    frames = []
+    for frame in frames_of_corner[corner]:
+        if view_mats[frame[0]] is not None:
+            frames.append(frame)
+
+    if len(frames) < 2:
+        return
+
+    max_dist = 0
+    best_frames = [None, None]
+    best_ids = [None, None]
+
+    for frame_1 in frames:
+        for frame_2 in frames:
+            if frame_1 == frame_2:
+                continue
+            tvec_1 = tvecs[frame_1[0]]
+            tvec_2 = tvecs[frame_2[0]]
+
+            d = np.linalg.norm(tvec_1 - tvec_2)
+            if max_dist < d:
+                max_dist = d
+                best_frames = [frame_1[0], frame_2[0]]
+                best_ids = [frame_1[1], frame_2[1]]
+
+    if max_dist > 0.01:
+        ids = np.array([corner])
+        points1 = corner_storage[best_frames[0]].points[np.array([best_ids[0]])]
+        points2 = corner_storage[best_frames[1]].points[np.array([best_ids[1]])]
+        correspondences = Correspondences(ids, points1, points2)
+        points3d, ids, _ = triangulate_correspondences(correspondences,
+                                                       view_mats[best_frames[0]],
+                                                       view_mats[best_frames[1]],
+                                                       intrinsic_mat,
+                                                       parameters=TriangulationParameters(2, 1e-3, 1e-4))
+        if len(points3d) > 0:
+            cloud[ids[0]] = points3d[0]
+            cur_corners_occurencies.pop(ids[0], None)
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
@@ -37,11 +131,99 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
     )
 
     # TODO: implement
+    num_frames_retr = 10
+    triang_params = TriangulationParameters(max_reprojection_error=8.0,
+                                            min_triangulation_angle_deg=1.0,
+                                            min_depth=.1)
+
     frame_count = len(corner_storage)
-    view_mats = [pose_to_view_mat3x4(known_view_1[1])] * frame_count
-    corners_0 = corner_storage[0]
-    point_cloud_builder = PointCloudBuilder(corners_0.ids[:1],
-                                            np.zeros((1, 3)))
+    view_mats = [None] * frame_count
+    tvecs = [None] * frame_count
+
+    frame_1 = known_view_1[0]
+    frame_2 = known_view_2[0]
+
+    tvecs[frame_1] = known_view_1[1].t_vec
+    tvecs[frame_2] = known_view_2[1].t_vec
+
+    view_mat_1 = pose_to_view_mat3x4(known_view_1[1])
+    view_mat_2 = pose_to_view_mat3x4(known_view_2[1])
+
+    view_mats[frame_1] = view_mat_1
+    view_mats[frame_2] = view_mat_2
+
+    cloud = {}
+
+    correspondences = build_correspondences(corner_storage[frame_1], corner_storage[frame_2])
+    points3d, ids, _ = triangulate_correspondences(correspondences,
+                                                   view_mat_1,
+                                                   view_mat_2,
+                                                   intrinsic_mat,
+                                                   triang_params)
+    for point3d, id in zip(points3d, ids):
+        cloud[id] = point3d
+
+    current_corners_occurences = {}
+    frames_of_corner = {}
+
+    for i, corners in enumerate(corner_storage):
+        for id_in_list, j in enumerate(corners.ids.flatten()):
+            current_corners_occurences[j] = 0
+            if j not in frames_of_corner.keys():
+                frames_of_corner[j] = [[i, id_in_list]]
+            else:
+                frames_of_corner[j].append([i, id_in_list])
+
+    while len(untracked_frames(view_mats)) > 0:
+        untr_frames = untracked_frames(view_mats)
+
+        max_num_inl = -1
+        best_frame = -1
+        best_rvec = None
+        best_tvec = None
+
+        for frame in untr_frames:
+            rvec, tvec, num_inl = calc_camera_pose(frame, corner_storage, cloud, intrinsic_mat)
+            if rvec is not None:
+                if num_inl > max_num_inl:
+                    best_frame = frame
+                    max_num_inl = num_inl
+                    best_rvec = rvec
+                    best_tvec = tvec
+
+        if max_num_inl == -1:
+            break
+
+        corners_to_add = []
+        for id in corner_storage[best_frame].ids.flatten():
+            if id in current_corners_occurences.keys():
+                current_corners_occurences[id] += 1
+                if current_corners_occurences[id] >= 2:
+                    corners_to_add.append(id)
+
+        for corner in corners_to_add:
+            add_new_point(corner, frames_of_corner, view_mats, tvecs, corner_storage, cloud, current_corners_occurences, intrinsic_mat)
+
+        print(f"Frame: {best_frame}, Inliers: {max_num_inl}")
+        print(f"Cloud size: {len(cloud)}")
+        view_mats[best_frame] = rodrigues_and_translation_to_view_mat3x4(best_rvec, best_tvec)
+        tvecs[best_frame] = best_tvec
+
+    last_mat = None
+    for nframe in range(frame_count):
+        if view_mats[i] is None:
+            view_mats[i] = last_mat
+        else:
+            last_mat = view_mats[i]
+
+    ids = []
+    points = []
+
+    for id in cloud.keys():
+        ids.append(id)
+        points.append(cloud[id])
+
+    point_cloud_builder = PointCloudBuilder(np.array(ids, dtype=np.int), np.array(points))
 
     calc_point_cloud_colors(
         point_cloud_builder,
--- camtrack/corners.py
+++ camtrack/corners.py
@@ -7,7 +7,8 @@ __all__ = [
     'dump',
     'load',
     'draw',
-    'without_short_tracks'
+    'without_short_tracks',
+    'filter_frame_corners'
 ]
 
 import click
@@ -16,7 +17,7 @@ import numpy as np
 import pims
 
 from _corners import FrameCorners, CornerStorage, StorageImpl
-from _corners import dump, load, draw, without_short_tracks, create_cli
+from _corners import dump, load, draw, without_short_tracks, create_cli, filter_frame_corners
 
 
 class _CornerStorageBuilder:
@@ -34,17 +35,91 @@ class _CornerStorageBuilder:
         return StorageImpl(item[1] for item in sorted(self._corners.items()))
 
 
+last_id = 0
+
+def calc_corners(image, max_level=1, old_corners=None):
+    feature_params = dict(blockSize=7,
+                          maxCorners=0,
+                          minDistance=7,
+                          qualityLevel=0.091,
+                          useHarrisDetector=False)
+
+    levels = [image]
+    points = np.empty((0, 1, 2), dtype=np.uint8)
+    sizes = np.empty((0), dtype=np.uint8)
+    ids = np.empty((0), dtype=np.uint8)
+
+    if old_corners is not None:
+        last_id = (old_corners.ids.ravel()[-1] + 1).astype(int)
+    else:
+        last_id = 0
+
+    for i in range(max_level):
+        levels.append(cv2.pyrDown(levels[-1]))
+
+    for counter, level in enumerate(levels):
+        scale = 2 ** counter
+
+        mask = np.zeros_like(level, np.uint8)
+        mask[:] = 255
+        if old_corners is not None:
+            points_on_level = old_corners.points[(old_corners.sizes == 7 * scale).reshape(-1)]
+            for point in points_on_level:
+                x, y = point / scale
+                cv2.circle(mask, (x, y), 7 * scale, 0, -1)
+
+        new_points = cv2.goodFeaturesToTrack(level, mask=mask, **feature_params)
+
+        if new_points is not None:
+            points = np.append(points, new_points * scale, axis=0)
+            sizes = np.append(sizes, (np.ones(len(new_points), dtype=int) * 7 * scale), axis=0)
+    ids = np.arange(last_id, last_id + len(points))
+    return ids, points, sizes
+
+
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
+    lk_params = dict(winSize=(15, 15),
+                     maxLevel=6,
+                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 20, 0.02))
+
+    # Create corners on two levels
     image_0 = frame_sequence[0]
+    ids, points, sizes = calc_corners(image_0)
     corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+        ids,
+        points,
+        sizes
     )
-    builder.set_corners_at_frame(0, corners)
+
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        next_points, st, err = cv2.calcOpticalFlowPyrLK(np.uint8(image_0 * 255), np.uint8(image_1 * 255),
+                                                        corners.points, None, **lk_params)
+
+        # backtrack
+        # corners_0_lvl1r, st, err = cv2.calcOpticalFlowPyrLK(np.uint8(image_1 * 255), np.uint8(image_0 * 255), corners_1_lvl1, None, **lk_params)
+        # error = abs(corners_0_lvl1 - corners_0_lvl1r).reshape(-1, 2).max(-1)
+        # corners_1_lvl1 = corners_1_lvl1[error < 1]
+        tracked_corners = filter_frame_corners(corners, st.reshape(-1) == 1)
+        builder.set_corners_at_frame(frame - 1, tracked_corners)
+
+        prev_points = FrameCorners(
+            tracked_corners.ids,
+            next_points[st.reshape(-1) == 1],
+            tracked_corners.sizes
+        )
+
+        ids, points, sizes = calc_corners(image_1, old_corners=prev_points)
+        ids = np.append(prev_points.ids.reshape(-1), ids, axis=0)
+        points = np.append(prev_points.points.reshape(-1, 2), points.reshape(-1, 2), axis=0)
+        sizes = np.append(prev_points.sizes.reshape(-1), sizes, axis=0)
+
+        corners = FrameCorners(
+            ids,
+            points,
+            sizes
+        )
+
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
 
